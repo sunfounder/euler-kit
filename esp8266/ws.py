6a9e2fa7d14986ad3120c7c40da756a71acf5a5f
Lovmong <1967471283@qq.com>
2023-03-15 16:54:16 +0800

    optimize

--- esp8266/ws.py
+++ esp8266/ws.py
@@ -3,58 +3,78 @@ import time
 import json
 
 from machine import Pin
-led = Pin(25, Pin.OUT)
+onboard_led_ws = Pin(25, Pin.OUT)
+
+"custom Exception"
+class TimeoutError(Exception):
+    pass
+
+LOG_FILE = "log.txt"
+
+def log(msg):
+    with open(LOG_FILE, "a") as log_f:
+        log_f.write(f'\n> {msg}')
+        time.sleep(0.01)
 
 class WS_Server():
-
+    WS_TIMEOUT = 3000 # ms
+    SEND_INTERVAL = 100 # ms
+    
     send_dict = {
         'Name': '',
-        'Type': 'PICO-4WD Car',
-        'Check': 'SunFounder Controller',
+        'Type': 'Euler Kit',
+        'Check': 'SC',
         }
 
     def __init__(self, name=None, ssid=None, password='', mode=None, port=8765):
         self.name = name
         self.ssid = ssid
+        if self.ssid == None or self.ssid == "":
+            self.ssid = name
         self.password = password
         self.mode = mode.lower()
         self.port = port
-        self.uart = UART(1, 115200, timeout=100, timeout_char=10)
+        self.uart = UART(1, 115200, timeout=10, timeout_char=5)
+
         self.listen_s = None
         self.client_s = None
         self.ws = None
         self.wlan = None
+        self._is_connected = False
+        # self.last_send_time = 0
 
         self.send_dict["Name"] = self.name
-
-        self.set("RESET")
+        print('reset ESP8266 module ...')
+        esp8266_version = self.set("RESET", timeout=2500)
+        print(f'ESP8266 module firmware version {esp8266_version}')
 
     def read(self, block=False):
         buf = ""
-        while 1: 
+        while True: 
             buf = self.uart.readline()
-            # print("buf: %s" % buf)
             if buf == None:
-                # print("Timeout")
                 if block:
-                    # time.sleep_ms(10)
+                    time.sleep_ms(1)
                     continue
                 else:
                     return None
-            if buf[0] == 0xff:
+
+            if buf[0] < 0x31 or buf[0] > 0xfe:
                 buf = buf[1:]
+                log("bufxx: %s" % buf)
+                if buf == '':
+                    buf = "GARBLED" # Garbled characters
+                    return buf
+                     
             buf = buf.decode().replace("\r\n", "")
-            # print("buf: %s" % buf)
             if buf.startswith("[DEBUG] "):
                 buf = buf.replace("[DEBUG]", "[ESP8266]")
-                # print(buf)
             else:
                 return buf
 
     def write(self, value):
         value = "%s\n" % value
         value = value.encode()
-        # print("ws write: %s" %value)
         self.uart.write(value)
 
     def send_data(self):
@@ -63,21 +83,81 @@ class WS_Server():
 
     def _command(self, mode, command, value=None):
         command += str(value) if value != None else ""
-        # print("Send command: %s" % command)
         command = "%s+%s" % (mode, command)
         self.write(command)
 
-    def set(self, command, value=None):
+    def set(self, command, value=None, timeout=None):
+        flag = 0
+        retry_count = 0
+        retry_max_count = 3
+        t_s = 0
+
+        # retry function
+        def retry():
+            nonlocal  t_s, retry_count
+            self._command("SET", command, value)
+            t_s = time.ticks_ms()
+            retry_count = retry_count + 1
+
+        # send command
         self._command("SET", command, value)
+        # get start time
+        t_s = time.ticks_ms()
+
         while True:
-            result = self.read(block=True)
-            # print("Result: %s" % result)
-            if result.startswith("[ERROR]"):
-                raise ValueError(result)
-            if result.startswith("[OK]"):
+            # onboard LED flash
+            if flag == 0:
+                onboard_led_ws.on()
+                flag = 1
+                time.sleep(0.1)
+            else:
+                onboard_led_ws.off()
+                flag = 0
+                time.sleep(0.1)
+
+            # Timeout handle
+            if timeout != None:
+                if(time.ticks_ms() - t_s > timeout):
+                    if retry_count < retry_max_count:
+                        log(f"TimeoutError. retry {retry_count} ...     ")
+                        retry()
+                        continue
+                    raise TimeoutError('Set timeout %s ms'%timeout)
+
+            # UnicodeError
+            try:
+                result = self.read(block=False)
+            except UnicodeError:
+                if retry_count < retry_max_count:
+                    log(f"UnicodeError retry {retry_count} ...")
+                    retry()
+                    continue
+                log("UnicodeError")
+            
+            # result
+            if result == None:
+                continue
+            elif result.startswith("[ERROR]"):
+                if retry_count < retry_max_count:
+                    log(f"{result} retry {retry_count} ...")
+                    retry()
+                    continue
+                log("result == [ERROR]")
+                
+            elif result == 'GARBLED':
+                if retry_count < retry_max_count:
+                    log(f"result GARBLED retry {retry_count} ...")
+                    retry()
+                    continue
+                log("result GARBLED ")
+
+            elif result.startswith("[OK]"):
                 result = result[4:]
                 result = result.strip(" ")
                 break
+            else:
+                continue
+
         return result
 
     def _get(self, command):
@@ -86,50 +166,70 @@ class WS_Server():
         return result
 
     def start(self):
-        led.high()
-        if self.mode == "ap":
-            self.set("SSID", self.name)
-            led.low()
-            self.set("PSK", self.password)
-            self.set("MODE", 2)
-        elif self.mode == "sta":
-            self.set("SSID", self.ssid)
-            led.low()
-            self.set("PSK", self.password)
-            self.set("MODE", 1)
-        
-        self.set("PORT", self.port)
-        print("Connecting")
         try:
-            ip = self.set("START")
+            if self.mode == "sta":
+                self.set("MODE", 1, timeout=self.WS_TIMEOUT)
+            elif self.mode == "ap":
+                self.set("MODE", 2, timeout=self.WS_TIMEOUT)
+            self.set("SSID", self.ssid, timeout=self.WS_TIMEOUT)
+            self.set("PSK", self.password, timeout=self.WS_TIMEOUT)
+            self.set("PORT", self.port, timeout=self.WS_TIMEOUT)
+        except TimeoutError as e:
+            print(e)
+            print("Configuring WiFi Timeout.Please check whether the ESP8266 module is working.")
+            return False
+        
+        try:
+            if self.mode == "sta":
+                print("Connecting to %s ... "%self.ssid)
+            elif self.mode == "ap":
+                print("open AP %s ... "%self.ssid)
+            ip = self.set("START", timeout=None)
             print("WebServer started on ws://%s:%d" % (ip, self.port))
+            return True
         except ValueError as e:
             print(e)
             print("Connect Wifi error. Try another Wifi or AP mode.")
+            return False
 
+    def is_connected(self):
+        return self._is_connected
+        
     def on_receive(self, data):
         pass
 
     def loop(self):
-        # print("waiting for uart data...")
         receive = self.read()
-        # print("Received.")
-        # print("ws loop, receive: %s" % receive)
+        # if receive is not None:
+        #     print(f"ws.loop received: {receive}")
+            
         if receive == None:
             self.send_data()
             return
         elif receive.startswith("[CONNECTED]"):
+            self._is_connected = True
             print("Connected from %s" % receive.split(" ")[1])
             self.send_data()
         elif receive.startswith("[DISCONNECTED]"):
+            self._is_connected = False
             print("Disconnected from %s" % receive.split(" ")[1])
+        elif receive.startswith("[APPSTOP]"):
+            self._is_connected = False
         else:
             try:
-                # print("on revceive: %s" % receive)
                 data = json.loads(receive)
                 if isinstance(data, str):
                     data = json.loads(data)
+                self._is_connected = True
                 self.on_receive(data)
+                self.send_data()
             except ValueError as e:
-                print(e)
-            self.send_data()
+                pass
+                # print("\033[0;31m[%s\033[0m"%e)
+        
+        # if (time.ticks_ms() - self.last_send_time > self.SEND_INTERVAL):
+        #     self.send_data()
+        #     self.last_send_time = time.ticks_ms()
+
+           
+
