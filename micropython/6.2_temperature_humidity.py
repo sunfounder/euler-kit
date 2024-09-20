93350e86712ef05abb03639a766458e977271a3b
xiemeiping <1035791926@qq.com>
2024-09-20 15:32:42 +0800

    Update 6.2_temperature_humidity.py

--- micropython/6.2_temperature_humidity.py
+++ micropython/6.2_temperature_humidity.py
@@ -1,11 +1,20 @@
-from machine import Pin, I2C
+from machine import Pin
 import utime as time
 from dht import DHT11
 
-pin = Pin(16, Pin.OUT, Pin.PULL_DOWN)
+# Initialize the pin for the DHT11 sensor as an input
+pin = Pin(16, Pin.IN)
+
+# Create a DHT11 sensor object
 sensor = DHT11(pin)
 
 while True:
+    # Measure temperature and humidity
     sensor.measure()
+    
+    # Print the measured temperature and humidity values
     print("Temperature: {}, Humidity: {}".format(sensor.temperature, sensor.humidity))
+    
+    # Wait for 1 second before the next measurement
     time.sleep(1)
+
