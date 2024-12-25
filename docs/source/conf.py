fa6fcf4f8c14298d7ac95d660c6e83de78af7e3e
xiemeiping <1035791926@qq.com>
2024-12-25 18:25:19 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,9 +34,14 @@ release = '1.0'
 # ones.
 extensions = [
     'sphinx.ext.autosectionlabel',
-    'sphinx_copybutton'
+    'sphinx_copybutton',
+    'sphinx_rtd_theme',
 ]
 
+# -- sphinx_rtd_theme Theme options -----------------------------------------------------
+html_theme_options = {
+    'flyout_display': 'attached'
+}
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
